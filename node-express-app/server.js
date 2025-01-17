27a250af96722a9770f520cc38f42619cfc10168
Sofia Neo <106977790+sofineo@users.noreply.github.com>
2025-01-16 19:25:34 -0800

    Update server.js

--- node-express-app/server.js
+++ node-express-app/server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello Professor, this is Sofia Neo - FSWD111: Week #4 assigment!");
 });
 
 app.get("/health", (req, res) => {
